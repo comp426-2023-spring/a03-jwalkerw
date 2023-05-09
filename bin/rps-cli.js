fd3ae34d5bcdfd7f5e83b4d24adfbe7ed969c0a8
Jordan Wahab <jordan_wahab@unc.edu>
2023-05-08 21:23:45 -0400

    Update rps-cli.js

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -17,6 +17,7 @@ if (options.h || options.help) {
     console.log('                  e.g. {"player":"rock"}');
     console.log("  node-rps rock   Return JSON with results for RPS played against a simulated opponent.");
     console.log('                  e.g {"player":"rock","opponent":"scissors","result":"win"}');
+    process.exit(0);
 }
 
 if (options.r || options.rules) {
@@ -25,5 +26,30 @@ if (options.r || options.rules) {
     console.log("  - Scissors CUTS Paper");
     console.log("  - Paper COVERS Rock");
     console.log("  - Rock CRUSHES Scissors");
+    process.exit(0);
 }
 
+let pcMove = options._[0];
+
+try {
+    let ans = rps(pcMove);
+    console.log(JSON.stringify(ans));
+} catch (e) {
+    console.log("Usage: node-rps [SHOT]");
+    console.log("Play Rock Paper Scissors (RPS)");
+    console.log("");
+    console.log("  -h, --help      display this help message and exit");
+    console.log("  -r, --rules     display the rules and exit");
+    console.log("");
+    console.log("Examples:");
+    console.log("  node-rps        Return JSON with single player RPS result.");
+    console.log('                  e.g. {"player":"rock"}');
+    console.log("  node-rps rock   Return JSON with results for RPS played against a simulated opponent.");
+    console.log('                  e.g {"player":"rock","opponent":"scissors","result":"win"}');
+    console.log("Rules for Rock Paper Scissors:");
+    console.log("");
+    console.log("  - Scissors CUTS Paper");
+    console.log("  - Paper COVERS Rock");
+    console.log("  - Rock CRUSHES Scissors");
+    process.exit(1);
+}
